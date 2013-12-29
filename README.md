688683678c1c5a6730df5b4f366f9cf69ac60c79
justin j. moses <justinjmoses@gmail.com>
2013-12-29 15:25:46 +0200

    Adding basic usage structure

--- README.md
+++ README.md
@@ -5,6 +5,24 @@ msw-subscribe
 
 Node application to scan for swell matching specific criteria at any break across the world. Uses [MSW](http://magicseaweed.com) forecast data. 
 
+
+##Quickstart
+
 ```javascript
-var example = require('example'); //pending
+//Add the API to your module
+var msw = require('msw-subscription');
+
+var subscription = mswSubscribe.create({
+  apiKey: 'YOUR API KEY',
+  spots: [1, 23, 55], //collection of spots to chck
+  query: {
+      minPeriod: 10,
+      minBreakingHeight: 3,
+      minSolidStars: 2,
+      maxWindSpeed: 15,
+      minSequence: 3
+  } //see msw-api package for query details
+});
+
+subscription.query().send({ email: 'recipient@example.com' });
 ```
---
+++ test/subscribe.spec.js
@@ -0,0 +1,86 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+
+var nock = require('nock');
+var mocks = require('../node_modules/msw-api/test/mocks.js');
+
+var subscribe = require('../lib/subscribe.js');
+
+
+describe('subscriber', function () {
+    var subscription;
+    var config = { apiKey: 'abcdef' };
+    var spots = [
+        {
+            id: 12345,
+            query: {
+                minPeriod: 10,
+                minBreakingHeight: 3,
+                minSolidStars: 2,
+                maxWindSpeed: 15
+            }
+        },
+        {
+            id: 12,
+            query: {
+                minFadedStars: 3
+            }
+        }
+    ];
+
+    describe('create()', function () {
+        it('must handle basic scenarios', function () {
+            subscription = subscribe.create(config);
+        });
+    });
+
+    describe('main functionality', function () {
+
+        //todo: port this to msw-api 0.0.9
+        function mockSpot(spotId, units, response) {
+            var mocked = nock('http://magicseaweed.com').get('/api/' + config.apiKey + '/forecast/?spot_id=' + spotId + '&units=' + units);
+            mocked.reply(response, (response === 200) ? mocks[spotId] : undefined);
+        }
+
+        beforeEach(function () {
+            subscription = subscribe.create(config);
+            spots.forEach(function (spotId) {
+                mockSpot(spotId, 'us', 200);
+            });
+        });
+
+        describe('addSpot()', function () {
+            it('must support adding a spot', function () {
+                spots.forEach(function (spot) {
+                    subscription.addSpot(spot.id, spot.query);
+                });
+            });
+        });
+
+        describe('querying & sending', function () {
+            beforeEach(function () {
+                spots.forEach(function (spot) {
+                    subscription.addSpot(spot.id, spots.query);
+                });
+            });
+
+            describe('query()', function () {
+                it('must query all if no parameter given', function () {
+                    subscription.query();
+                });
+            });
+
+            describe('send()', function () {
+                beforeEach(function () {
+                    subscription.query(); //load all forecasts
+                });
+
+                it('must send emails as required', function () {
+                    subscription.send({ email: 'test@example.com' });
+                });
+            });
+        });
+    });
+});
